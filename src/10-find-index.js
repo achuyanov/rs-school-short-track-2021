42d765ecc8f23d040225646ea89753643138e137
AndreiChuyanov <achuyanov@gmail.com>
2021-04-12 22:41:15 +0300

    feat: task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,9 +1,9 @@
 /**
- * Given a sorted array, find the index of the element with the given value.
+ * Given a sorted array, find the index of the element with the given val.
  * Time complexity should be O(logN)
  *
  * @param {Array} array
- * @param {Number} value
+ * @param {Number} val
  * @return {Number}
  *
  * @example
@@ -11,8 +11,18 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, val) {
+  let start = 0;
+  let end = array.length;
+  let pos = Math.floor(end / 2);
+
+  while (pos >= 0) {
+    if (array[pos] === val) return pos;
+    if (val < array[pos]) {end = pos} else {start = pos}
+    pos = Math.floor((start + end) / 2);
+  }
+
+  return pos;
 }
 
 module.exports = findIndex;
