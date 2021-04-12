415d56dc9346020de131dccf21347762bb116799
AndreiChuyanov <achuyanov@gmail.com>
2021-04-12 21:37:24 +0300

    feat: task 12 solution

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,22 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(arr) {
+  let srt = [];
+  arr.map(x=>x.split('.').reverse()).forEach(el=>{
+    let len = el.length;
+    srt.push(el[0]);
+    if (len > 1) srt.push(el[0]+'.'+el[1]);
+    if (len > 2) srt.push(el.join('.'));
+  })
+  srt = srt.map(x=>'.'+x);
+
+  return srt.reduce((acc, val) => ({...acc,[val]: (acc[val] || 0) + 1 }), {});
 }
+
+
+console.log(getDNSStats(['epam.com', 'info.epam.com']));
+
+
 
 module.exports = getDNSStats;
